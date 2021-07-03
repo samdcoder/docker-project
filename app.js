34ac76bdbcf33e8ba9aa47b25f01239e3cc5b992
Sameer Pathan <PEOPLE\sameerpathan@lp8415.people.local>
2021-07-04 02:49:52 +0530

    redirect

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+http.get('*', function(req, res) {  
+    res.redirect('http://' + req.headers.host + req.url);
+
+    // Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
+    // res.redirect('https://example.com' + req.url);
+})
+
 app.get('/', (req, res) => {
 	return res.send('<html> <img width="100%" height="100%" src="https://picsum.photos/200" /> </ 	html>');
 })
